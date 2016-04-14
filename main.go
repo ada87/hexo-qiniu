c96858e10fd63ae29d5b5ec9f82cbffda49353dc
xiangda <ada87@163.com>
2016-04-14 14:06:14 +0800

    Signed-off-by: xiangda <ada87@163.com>

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/qiniu/api.v7/kodo"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/qiniu/api.v7/kodo"
 	"qiniupkg.com/api.v7/conf"
 	"qiniupkg.com/api.v7/kodocli"
-	"strings"
 )
 
 //构造返回值字段
@@ -21,19 +22,16 @@ func main() {
 	conf.SECRET_KEY = Config.Sk
 	//创建一个Client
 	client := kodo.New(0, nil)
-	policy := &kodo.PutPolicy{
-		Scope:      Config.Space,
-		Expires:    3600,
-		InsertOnly: 1,
-	}
-
-	token := client.MakeUptoken(policy)
 	uploader := kodocli.NewUploader(0, nil)
 	var ret PutRet
 	filepath.Walk(Config.Path, func(filename string, fi os.FileInfo, err error) error {
 		if !fi.IsDir() {
 			target := strings.Replace(filename, "\\", "/", -1)
 			target = strings.Split(target, "/public/")[1]
+			policy := &kodo.PutPolicy{
+				Scope: Config.Space + ":" + target,
+			}
+			token := client.MakeUptoken(policy)
 			fmt.Println(target)
 			fmt.Println(filename)
 			res := uploader.PutFile(nil, &ret, token, target, filename, nil)
